ae03c4893976a3bcfd636db5dff5d37fc3d94a2b
Jonyigit <jonyigitavazbekov@gmail.com>
2025-06-23 16:33:05 +0300

    add loading animation

--- src/widgets/Home/ui/Home.jsx
+++ src/widgets/Home/ui/Home.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
 import Router from "../../../app/Router/Router";
+import styles from "./Home.module.scss";
 
 function Home() {
-    return <Router />;
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => setLoading(true), 3000);
+        return () => clearTimeout(timer);
+    }, []);
+
+    return loading ? (
+        <Router />
+    ) : (
+        <div className={styles.loading}>
+            <div class={styles.spinner}>
+                <div class={styles.spinnerin}></div>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
