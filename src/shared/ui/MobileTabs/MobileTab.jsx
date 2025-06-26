37102d6246cbb6cd890826b653b8b6f0cee61a84
Jonyigit <jonyigitavazbekov@gmail.com>
2025-06-26 09:18:13 +0300

    End of Project

--- src/shared/ui/MobileTabs/MobileTab.jsx
+++ src/shared/ui/MobileTabs/MobileTab.jsx
@@ -1,19 +1,28 @@
+import { NavLink, useLocation } from "react-router-dom";
 import clsx from "clsx";
 import styles from "./MobileTabs.module.scss";
 
-function MobileTab(props) {
-    const { activeList, handleListChange } = props;
+function MobileTab() {
+    const { pathname } = useLocation();
+    const name = pathname.split("/")[1];
+
+    const tabs = ["overview", "internal", "surface"];
+
     return (
         <ul className={styles.mobileTabs}>
-            {["overview", "internal", "surface"].map((item) => (
-                <li
+            {tabs.map((item) => (
+                <NavLink
+                    to={`/${name}/${item}`}
                     key={item}
-                    className={clsx(activeList === item && styles.mobileTabs__itemActive)}
-                    onClick={() => handleListChange(item)}
+                    className={({ isActive }) =>
+                        clsx(styles.mobileTabs__item, {
+                            [styles.mobileTabs__itemActive]: isActive,
+                        })
+                    }
                 >
                     <span>{item === "internal" ? "Structure" : item[0].toUpperCase() + item.slice(1)}</span>
                     <div className={styles.mobileTabs__underline}></div>
-                </li>
+                </NavLink>
             ))}
         </ul>
     );
